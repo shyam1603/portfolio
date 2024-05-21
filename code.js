0daa278042985ba690dd4bf6a642e43b5f466167
Shyamsundar Dandapat <145271748+shyam1603@users.noreply.github.com>
2024-05-21 20:07:30 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,3 @@
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('Thank you for reaching out! Please connect with me on LinkedIn or send an email to example@example.com.');
+});
